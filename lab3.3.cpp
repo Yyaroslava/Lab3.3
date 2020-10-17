8978cfd6d35acaa0a4ee005e0f96ca414bcda76c
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-10-17 17:39:18 +0300

    Add files via upload

---
+++ lab3.3.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    double x;  // вхідний аргумент 
+    double R;  // вхідний параметр
+    double y;  // результат обчислення виразу 
+
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
+    // розгалуження в повній формі  
+    if (x <= -8 - R)
+        y = R;
+    else
+        if (-8 - R <= x && x <= -8 + R)
+            y = R - sqrt(R * R - (x + 8) * (x + 8));
+        else
+            if (-8 + R <= x && x <= -4)
+                y = R;
+            else
+                if (-4 <= x && x <= 2)
+                    y = R + (-1 - R) / 6 * (x + 4);
+                else
+                    y = x;
+
+    cout << endl;
+    cout << "y = " << y << endl;
+
+    cin.get();
+    return 0;
+}
